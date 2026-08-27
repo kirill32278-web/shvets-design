2beb9bcfbab3d1df46867b141276bb8cb2090d5e
shvets <kirill32.278@gmail.com>
2026-08-28 00:23:34 +0300

    Temporary inspect image blobs

--- assets/README-restore.txt
+++
@@ -1,1 +0,0 @@
-High-resolution gallery restoration in progress.
--- data/new/b14.js
+++
@@ -1,5 +0,0 @@
-document.write('<script src="data/new/a-yo-galstuk.js"><\/script>');
-document.write('<script src="data/new/genesis.js"><\/script>');
-document.write('<script src="data/new/kreslo-88.js"><\/script>');
-document.write('<script src="data/new/proekt-komoda.js"><\/script>');
-document.write('<script src="data/new/ledokol.js"><\/script>');
