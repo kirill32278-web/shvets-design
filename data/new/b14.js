9a57517d9b93c0ca74062c14972b7cec55b777d9
shvets <kirill32.278@gmail.com>
2026-08-28 00:33:25 +0300

    Finish catalog and improve key portfolio images

--- data/new/b14.js
+++ data/new/b14.js
@@ -3,3 +3,4 @@ document.write('<script src="data/new/genesis.js"><\/script>');
 document.write('<script src="data/new/kreslo-88.js"><\/script>');
 document.write('<script src="data/new/proekt-komoda.js"><\/script>');
 document.write('<script src="data/new/ledokol.js"><\/script>');
+document.write('<script src="data/hq-loader.js"><\/script>');
