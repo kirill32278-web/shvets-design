69ddca41c71b4a9e8efbd19de03843aad5b37768
shvets <kirill32.278@gmail.com>
2026-08-28 09:13:14 +0300

    Enable full galleries for new catalog objects

--- data/new/b14.js
+++ data/new/b14.js
@@ -4,3 +4,4 @@ document.write('<script src="data/new/kreslo-88.js"><\/script>');
 document.write('<script src="data/new/proekt-komoda.js"><\/script>');
 document.write('<script src="data/new/ledokol.js"><\/script>');
 document.write('<script src="data/hq-loader.js"><\/script>');
+document.write('<script src="data/sprite-gallery.js"><\/script>');
